b48ddf1d047dd2d7a5f30e08cdfa0fabc6ad24ed
maxidea1024@gmail.com <maxidea1024@gmail.com>
2020-01-08 14:26:57 +0900

    Update ScreenFlash.cs

--- UnityScreenFlash/Assets/ScreenFlash/ScreenFlash.cs
+++ UnityScreenFlash/Assets/ScreenFlash/ScreenFlash.cs
@@ -6,26 +6,26 @@ using UnityEngine.Assertions;
 
 public class ScreenFlash : MonoBehaviour
 {
-	//TODO ease 함수를 적용할 수 있도록 하는것도 좋을듯..
+    //TODO ease 함수를 적용할 수 있도록 하는것도 좋을듯..
 
-	//
-	// Public methods
-	//
+    //
+    // Public methods
+    //
 
-	public static void Flash(Color color, float duration)
-	{
-		instance.AddItem(Time.time, color, duration * 0.5f, 0f, duration * 0.5f);
-	}
+    public static void Flash(Color color, float duration)
+    {
+        instance.AddItem(Time.time, color, duration * 0.5f, 0f, duration * 0.5f);
+    }
 
-	public static void Flash(Color color, float inDuration, float outDuration)
-	{
-		instance.AddItem(Time.time, color, inDuration, 0f, outDuration);
-	}
+    public static void Flash(Color color, float inDuration, float outDuration)
+    {
+        instance.AddItem(Time.time, color, inDuration, 0f, outDuration);
+    }
 
-	public static void Flash(Color color, float inDuration, float holdDuration, float outDuration)
-	{
-		instance.AddItem(Time.time, color, inDuration, holdDuration, outDuration);
-	}
+    public static void Flash(Color color, float inDuration, float holdDuration, float outDuration)
+    {
+        instance.AddItem(Time.time, color, inDuration, holdDuration, outDuration);
+    }
 
     public static void Cancel()
     {
@@ -35,15 +35,15 @@ public class ScreenFlash : MonoBehaviour
 
 
 
-	//
-	// Implementation
-	//
+    //
+    // Implementation
+    //
 
-	void Awake()
-	{
-		instance = this;
-		gameObject.SetActive(false);
-	}
+    void Awake()
+    {
+        instance = this;
+        gameObject.SetActive(false);
+    }
 
 
     struct Item
@@ -57,13 +57,13 @@ public class ScreenFlash : MonoBehaviour
 
     void AddItem(float timePoint, Color color, float inDuration, float holdDuration, float outDuration)
     {
-		Assert.IsTrue(inDuration >= 0f);
-		Assert.IsTrue(holdDuration >= 0f);
-		Assert.IsTrue(outDuration >= 0f);
-		Assert.IsTrue(inDuration > 0f || holdDuration > 0f || outDuration > 0f);
+        Assert.IsTrue(inDuration >= 0f);
+        Assert.IsTrue(holdDuration >= 0f);
+        Assert.IsTrue(outDuration >= 0f);
+        Assert.IsTrue(inDuration > 0f || holdDuration > 0f || outDuration > 0f);
 
-		// Master alpha scaling.
-		color.a *= masterAlphaScale;
+        // Master alpha scaling.
+        color.a *= masterAlphaScale;
 
         Item item = new Item {
             color = color,
@@ -75,23 +75,23 @@ public class ScreenFlash : MonoBehaviour
 
         items.Add(item);
 
-		if (!gameObject.activeSelf)
-		{
-			gameObject.SetActive(true);
+        if (!gameObject.activeSelf)
+        {
+            gameObject.SetActive(true);
 
-			StartCoroutine(CoFlashing());
-		}
+            StartCoroutine(CoFlashing());
+        }
     }
 
-	bool BlendItemColors(float timePoint, out Color result)
-	{
-		int blendedCount = 0;
+    bool BlendItemColors(float timePoint, out Color result)
+    {
+        int blendedCount = 0;
 
-		// Warning: initial color's alpha element must be 0.
-		result = new Color(0, 0, 0, 0);
+        // Warning: initial color's alpha element must be 0.
+        result = new Color(0, 0, 0, 0);
 
-		for (int i = 0; i < items.Count; ++i)
-		{
+        for (int i = 0; i < items.Count; ++i)
+        {
             if (GetItemColorAtTime(items[i], timePoint, out Color itemColor))
             {
                 result = ColorBlender.Blend(itemColor, result);
@@ -104,8 +104,8 @@ public class ScreenFlash : MonoBehaviour
             }
         }
 
-		return blendedCount == 0; // Returns whether done or not.
-	}
+        return blendedCount == 0; // Returns whether done or not.
+    }
 
     //bool IsExpiredItem(Item item, float timePoint)
     //{
@@ -119,59 +119,59 @@ public class ScreenFlash : MonoBehaviour
         float duration = item.inDuration + item.holdDuration + item.outDuration;
         float elapsedTime = timePoint - item.addedTime;
 
-		if (elapsedTime >= duration)
-		{
-			// Expired!
-			result = Color.black;
-			return false;
-		}
+        if (elapsedTime >= duration)
+        {
+            // Expired!
+            result = Color.black;
+            return false;
+        }
 
 
-		float alphaScale;
+        float alphaScale;
 
-		if (item.inDuration > 0f && elapsedTime < item.inDuration)
-		{
-			alphaScale = elapsedTime / item.inDuration;
-		}
-		else if (item.holdDuration > 0f && elapsedTime < (item.inDuration + item.holdDuration))
-		{
-			alphaScale = 1f;
-		}
-		else if (item.outDuration > 0f)
-		{
-			alphaScale = 1f - ((elapsedTime - item.inDuration - item.holdDuration) / item.outDuration);
-		}
-		else
-		{
-			// Some duration paramters are wrong.
-			result = Color.black;
-			return false;
-		}
+        if (item.inDuration > 0f && elapsedTime < item.inDuration)
+        {
+            alphaScale = elapsedTime / item.inDuration;
+        }
+        else if (item.holdDuration > 0f && elapsedTime < (item.inDuration + item.holdDuration))
+        {
+            alphaScale = 1f;
+        }
+        else if (item.outDuration > 0f)
+        {
+            alphaScale = 1f - ((elapsedTime - item.inDuration - item.holdDuration) / item.outDuration);
+        }
+        else
+        {
+            // Some duration paramters are wrong.
+            result = Color.black;
+            return false;
+        }
 
         //alphaScale = EasingFunction.EaseInBounce(0f, 1f, alphaScale);
 
-		result = new Color(item.color.a, item.color.g, item.color.b, item.color.a * alphaScale);
+        result = new Color(item.color.a, item.color.g, item.color.b, item.color.a * alphaScale);
 
         return true;
     }
 
-	IEnumerator CoFlashing()
-	{
-		while (true)
-		{
-			yield return null;
+    IEnumerator CoFlashing()
+    {
+        while (true)
+        {
+            yield return null;
 
             bool done = BlendItemColors(Time.time, out Color color);
             if (done)
-			{
-				break;
-			}
+            {
+                break;
+            }
 
-			flashCover.color = color;
-		}
+            flashCover.color = color;
+        }
 
-		gameObject.SetActive(false);
-	}
+        gameObject.SetActive(false);
+    }
 
 
     //
@@ -181,6 +181,6 @@ public class ScreenFlash : MonoBehaviour
     static ScreenFlash instance;
     readonly List<Item> items = new List<Item>();
 
-	public float masterAlphaScale = 1f;
+    public float masterAlphaScale = 1f;
     [SerializeField] Image flashCover;
 }
